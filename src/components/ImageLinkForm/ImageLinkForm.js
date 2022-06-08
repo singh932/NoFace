28b0c9f4495291b1936e2a44a9275c3da00ce2b8
Sattvik Palta <sattvikpalta@gmail.com>
2022-06-08 01:17:48 -0400

    Aligns items

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -3,13 +3,13 @@ import "./ImageLinkForm.css";
 
 const ImageLinkForm = () => {
   return (
-    <div>
+    <div className="form-container">
       <p className="f3">
         NoFace will detect faces in your pictures. Give it a try!
       </p>
       <div className="center">
-        <div className="form center pa4 shadow-5">
-          <input className="f4 pa2 w-70 center" type="text" />
+        <div className="form pa4 shadow-5">
+          <input className="f4 pa2 w-70" type="text" />
           <button className="f4 pa2 w-30 grow link dim ph3 pv2 dib white bg-light-pink">
             Detect
           </button>
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./Rank.css";
 
 const Rank = () => {
   return (
-    <div>
+    <div className="rank-container">
       <div className="white f3">{"Sattvik your current rank is..."}</div>
       <div className="white f1">{"#5"}</div>
     </div>
