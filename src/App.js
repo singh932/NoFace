dceb9f09445dc23b0e4ce6d72c9bd0dd8628bed4
Sattvik Palta <sattvikpalta@gmail.com>
2022-06-09 20:54:59 -0400

    Adds Clarifai API integration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
+import Clarifai from "clarifai";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
-import Rank from "./components/Rank/Rank";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
 import "./App.css";
+
+window.process = {
+  ...window.process,
+};
+
+const app = new Clarifai.App({
+  apiKey: "4558654d0fc64b7499eaf0c94d8f1032",
+});
+
+const particlesInit = async (main) => await loadFull(main);
+const particlesLoaded = (container) => "";
 
 const particlesOptions = {
   fpsLimit: 120,
@@ -75,14 +88,75 @@ const particlesOptions = {
   detectRetina: true,
 };
 
-const particlesInit = async (main) => await loadFull(main);
-const particlesLoaded = (container) => "";
-
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      imageUrl: "",
+    };
+  }
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  };
+
+  // Method 1: Using older way of fetching data from Clarifai API
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+    app.models
+      .predict("a403429f2ddf4b49b307e318f00e528b", this.state.input)
+      .then(
+        function (response) {
+          console.log(
+            response.outputs[0].data.regions[0].region_info.bounding_box
+          );
+        },
+        function (err) {}
+      );
+  };
+
+  // Method 1: Using newer way of fetching data from Clarifai API
+  // onButtonSubmit = () => {
+  // const raw = JSON.stringify({
+  //   user_app_id: {
+  //     user_id: "ukurv5kcd7h0",
+  //     app_id: "249915634c2847c4af05788ea2c968fa",
+  //   },
+  //   inputs: [
+  //     {
+  //       data: {
+  //         image: {
+  //           url: this.state.input,
+  //         },
+  //       },
+  //     },
+  //   ],
+  // });
+
+  // const requestOptions = {
+  //   method: "POST",
+  //   headers: {
+  //     Accept: "application/json",
+  //     Authorization: "Key 9b0cb5f74e254a26afa248f216ec57df",
+  //   },
+  //   body: raw,
+  // };
+
+  // fetch(
+  //   "https://api.clarifai.com/v2/models/a403429f2ddf4b49b307e318f00e528b/outputs",
+  //   requestOptions
+  // )
+  //   .then((response) => console.log(response))
+  //   .then((result) => console.log(result))
+  //   .catch((error) => console.log("error ", error));
+  // };
+
   render() {
     return (
       <div className="App">
         <Particles
+          className="particles"
           id="tsparticles"
           init={particlesInit}
           loaded={particlesLoaded}
@@ -91,8 +165,11 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/* <FaceRecognition /> */}
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     );
   }
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./ImageLinkForm.css";
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div className="form-container">
       <p className="f3">
@@ -9,8 +9,11 @@ const ImageLinkForm = () => {
       </p>
       <div className="center">
         <div className="form pa4 shadow-5">
-          <input className="f4 pa2 w-70" type="text" />
-          <button className="f4 pa2 w-30 grow link dim ph3 pv2 dib white">
+          <input className="f4 pa2 w-70" type="text" onChange={onInputChange} />
+          <button
+            className="f4 pa2 w-30 grow link dim ph3 pv2 dib white"
+            onClick={onButtonSubmit}
+          >
             Detect
           </button>
         </div>
